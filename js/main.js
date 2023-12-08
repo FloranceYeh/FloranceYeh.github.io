89fbd674f6b473da9228ed475024e651b045d5a3
FloranceYeh <Florance_Sunrise@outlook.com>
2023-12-08 12:08:33 +0000

    Site updated: 2023-12-08 12:08:31

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const app = Vue.createApp({
+    mixins: Object.values(mixins),
+    data() {
+        return {
+            loading: true,
+            hiddenMenu: false,
+            showMenuItems: false,
+            menuColor: false,
+            scrollTop: 0,
+            renderers: [],
+        };
+    },
+    created() {
+        window.addEventListener("load", () => {
+            this.loading = false;
+        });
+    },
+    mounted() {
+        window.addEventListener("scroll", this.handleScroll, true);
+        this.render();
+    },
+    methods: {
+        render() {
+            if (typeof this.renderers === "undefined") return;
+            for (let i of this.renderers) i();
+        },
+        handleScroll() {
+            let wrap = this.$refs.homePostsWrap;
+            let newScrollTop = document.documentElement.scrollTop;
+            if (this.scrollTop < newScrollTop) {
+                this.hiddenMenu = true;
+                this.showMenuItems = false;
+            } else this.hiddenMenu = false;
+            if (wrap) {
+                if (newScrollTop <= window.innerHeight - 100) this.menuColor = true;
+                else this.menuColor = false;
+                if (newScrollTop <= 400) wrap.style.marginTop = -newScrollTop / 5 + "px";
+                else wrap.style.marginTop = "-80px";
+            }
+            this.scrollTop = newScrollTop;
+        },
+    },
+});
+app.mount("#layout");
